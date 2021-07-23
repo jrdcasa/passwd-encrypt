b973124a7e58dfa7ea92440cc4102a5f701be7b4
jramos <jrdcasa>
2021-07-23 19:20:20 +0200

    second commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from os import path
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
